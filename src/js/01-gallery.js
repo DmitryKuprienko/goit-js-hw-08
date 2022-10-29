ed407ab33005ff04542e23e607e52114149b2eb7
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2022-10-30 00:33:29 +0300

    comitt 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
 
 console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryMarkup = createsGalleryLayout(galleryItems);
+galleryRef.insertAdjacentHTML("afterbegin", galleryMarkup);
+galleryRef.addEventListener("click", onClickImg);
+
+
+
+let lightbox = new SimpleLightbox('.gallery a',{
+  captionsData: "alt",
+  captionDelay: 250,
+    
+});
+
+function onClickImg(event) {
+  event.preventDefault();  
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  lightbox.open();
+}
+
+
+function createsGalleryLayout(galleryItems) {
+  return galleryItems
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join("");
+}
+console.log(galleryRef)
