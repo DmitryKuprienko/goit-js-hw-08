c3998f036bab0623e8630a933312d23eaba8e4ae
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2022-10-30 16:00:18 +0200

    commit 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+console.log(Player)
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
 
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+
+
+const onPlay = function(data) {
+    console.log(data)
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
