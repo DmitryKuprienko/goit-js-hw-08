b4b94bedebd2b2493ca97fae5a139c3c59e6bf66
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2022-10-31 23:38:34 +0200

    commit31102338

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,22 @@
 import Player from '@vimeo/player';
-console.log(Player)
-const player = new Player('handstick', {
-    id: 19231868,
-    width: 640
+
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const jeson=localStorage.getItem('videoplayer-current-time')
+
+player.on('timeupdate', function (date) {
+  jeson = localStorage.setItem('videoplayer-current-time', JSON.stringify(date));
 });
+console.log(jeson)
 
-player.on('play', function() {
-    console.log('played the video!');
+player.getVideoTitle().then(function () {
+  player.on(`timeupdate`, function () {
+    JSON.parse(jeson)
+  });
+
+  
+
+  console.log(localStorage.getItem('videoplayer-current-time'));
 });
-
-console.log(player)
-
-const onPlay = function(data) {
-    console.log(player)
-    
-};
-
-player.on('play', onPlay);
