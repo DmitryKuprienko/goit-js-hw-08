d31f864ac0a28184e2f8aafc1363c69dbda829e3
DmitryKuprienko <101987031+DmitryKuprienko@users.noreply.github.com>
2022-11-06 13:03:32 +0200

    task 02 complite

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,34 @@
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-const jeson=localStorage.getItem('videoplayer-current-time')
+const onPlay = function (date) {
 
-player.on('timeupdate', function (date) {
-  jeson = localStorage.setItem('videoplayer-current-time', JSON.stringify(date));
+localStorage.setItem('videoplayer-current-time', JSON.stringify(date));
+};
+
+player.on('timeupdate', throttle(onPlay,1000))
+ 
+const locStor= localStorage.getItem('videoplayer-current-time');
+const parseLocStor= JSON.parse(locStor);
+
+player.setCurrentTime(parseLocStor.seconds).then(function(seconds) { 
+   // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+  switch (error.name) {
+      case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+      default:
+          // some other error occurred
+          break;
+  }
 });
-console.log(jeson)
-
-player.getVideoTitle().then(function () {
-  player.on(`timeupdate`, function () {
-    JSON.parse(jeson)
-  });
 
   
 
-  console.log(localStorage.getItem('videoplayer-current-time'));
-});
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,7 @@
+import throttle from 'lodash.throttle';
 
+
+let getEL= x=> document.querySelector(x)
+
+const form = getEL(form);
+console.log(form)
